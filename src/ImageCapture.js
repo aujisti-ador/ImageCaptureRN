3f2ce15d9af954dd9019675e6b6dced83d04e19a
Fazle Rabbi Ador <fazle.rabbi@circlefintech.com>
2019-04-03 16:02:19 +0600

    camera added

--- src/ImageCapture.js
+++ src/ImageCapture.js
@@ -1,20 +1,176 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+/* eslint-disable no-console */
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Slider, CameraRoll, PermissionsAndroid } from 'react-native';
+// eslint-disable-next-line import/no-unresolved
+import { RNCamera } from 'react-native-camera';
 
-class ImageCapture extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
+const flashModeOrder = {
+  off: 'on',
+  on: 'auto',
+  auto: 'torch',
+  torch: 'off',
+};
+
+const wbOrder = {
+  auto: 'sunny',
+  sunny: 'cloudy',
+  cloudy: 'shadow',
+  shadow: 'fluorescent',
+  fluorescent: 'incandescent',
+  incandescent: 'auto',
+};
+
+export default class ImageCapture extends React.Component {
+  state = {
+    flash: 'off',
+    autoFocus: 'on',
+    type: 'back',
+    whiteBalance: 'auto',
+    ratio: '16:9',
+  };
+
+  toggleFacing() {
+    this.setState({
+      type: this.state.type === 'back' ? 'front' : 'back',
+    });
+  }
+
+  toggleFlash() {
+    this.setState({
+      flash: flashModeOrder[this.state.flash],
+    });
+  }
+
+  toggleWB() {
+    this.setState({
+      whiteBalance: wbOrder[this.state.whiteBalance],
+    });
+  }
+
+  takePicture = async function() {
+    const options = { quality: 0.1, base64: true };
+          const data = await this.camera.takePictureAsync(options);
+          let saveResult = CameraRoll.saveToCameraRoll(data.uri);
+          console.warn("takePicture ", saveResult);
+          console.warn("picture url ", data.uri);
+  };
+
+  renderCamera() {
+    return (
+      <RNCamera
+        ref={ref => {
+          this.camera = ref;
+        }}
+        style={{
+          flex: 1,
+        }}
+        type={this.state.type}
+        flashMode={this.state.flash}
+        autoFocus={this.state.autoFocus}
+        whiteBalance={this.state.whiteBalance}
+        ratio={this.state.ratio}
+        permissionDialogTitle={'Permission to use camera'}
+        permissionDialogMessage={'We need your permission to use your camera phone'}
+        onGoogleVisionBarcodesDetected={({ barcodes }) => {
+          console.log(barcodes);
+          // conditional part for barcode
+          // if (barcodes) {
+          //   this.props.navigation.navigate('ButtonRoute')
+          // } else {
+          //   console.log(barcodes);
+          // }
+        }}
+      >
+        <View
+          style={{
+            flex: 0.5,
+            backgroundColor: 'transparent',
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+          }}
+        >
+          <TouchableOpacity style={styles.flipButton} onPress={this.toggleFacing.bind(this)}>
+            <Text style={styles.flipText}> FLIP </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.flipButton} onPress={this.toggleFlash.bind(this)}>
+            <Text style={styles.flipText}> FLASH: {this.state.flash} </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.flipButton} onPress={this.toggleWB.bind(this)}>
+            <Text style={styles.flipText}> WB: {this.state.whiteBalance} </Text>
+          </TouchableOpacity>
+        </View>
+        <View
+          style={{
+            flex: 0.4,
+            backgroundColor: 'transparent',
+            flexDirection: 'row',
+            alignSelf: 'flex-end',
+          }}
+        >
+        </View>
+        <View
+          style={{
+            flex: 0.1,
+            backgroundColor: 'transparent',
+            flexDirection: 'row',
+            alignSelf: 'flex-end',
+          }}
+        >
+        </View>
+        <View
+          style={{
+            flex: 0.1,
+            backgroundColor: 'transparent',
+            flexDirection: 'row',
+            alignSelf: 'flex-end',
+          }}
+        >
+          <TouchableOpacity
+            style={[styles.flipButton, styles.picButton, { flex: 0.3, alignSelf: 'flex-end' }]}
+            onPress={this.takePicture.bind(this)}
+          >
+            <Text style={styles.flipText}> SNAP </Text>
+          </TouchableOpacity>
+        </View>
+      </RNCamera>
+    );
   }
 
   render() {
-    return (
-      <View>
-        <Text> Camera </Text>
-      </View>
-    );
+    return <View style={styles.container}>{this.renderCamera()}</View>;
   }
 }
 
-export default ImageCapture;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 10,
+    backgroundColor: '#000',
+  },
+  flipButton: {
+    flex: 0.3,
+    height: 40,
+    marginHorizontal: 2,
+    marginBottom: 10,
+    marginTop: 20,
+    borderRadius: 8,
+    borderColor: 'white',
+    borderWidth: 1,
+    padding: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  flipText: {
+    color: 'white',
+    fontSize: 15,
+  },
+  zoomText: {
+    position: 'absolute',
+    bottom: 70,
+    zIndex: 2,
+    left: 2,
+  },
+  picButton: {
+    backgroundColor: 'darkseagreen',
+  },
+});
