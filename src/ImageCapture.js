909ed2d67908e7c9174dc3373480b04b747b08a8
Redwan-ul Ansari <redwan-ulansari@Redwan-uls-iMac.local>
2019-04-03 14:39:46 +0600

    ImageCapture component screen added

---
+++ src/ImageCapture.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class ImageCapture extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> Camera </Text>
+      </View>
+    );
+  }
+}
+
+export default ImageCapture;
